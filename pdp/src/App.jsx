57b64fc5bd7553b31fbc11c3b93a57aac56310d4
Arpit Mehrotra <arpitmehrotra001@gmail.com>
2023-01-29 16:49:32 +0530

    sharing functions within apps, configure route

--- pdp/src/App.jsx
+++ pdp/src/App.jsx
@@ -1,16 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import "./index.scss";
 
 import Header from "home/Header";
 import Footer from "home/Footer";
+import PDPContent from "./PDPContent";
 
 const App = () => (
-  <div className="text-3xl mx-auto max-w-6xl">
-    <Header />
-    <div className="my-10">PDP Page Content</div>
-    <Footer />
-  </div>
+  <Router>
+    <div className="text-3xl mx-auto max-w-6xl">
+      <Header />
+      <div className="my-10">
+        <Routes>
+          <Route path="/products/:id" element={<PDPContent />} />
+        </Routes>
+      </div>
+      <Footer />
+    </div>
+  </Router>
 );
 ReactDOM.render(<App />, document.getElementById("app"));
