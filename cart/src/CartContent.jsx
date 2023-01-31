57145068d2e83f144b12d70ee4fdaddd58fcd47f
Arpit Mehrotra <arpitmehrotra001@gmail.com>
2023-01-31 09:52:15 +0530

    sharing the cart

--- cart/src/CartContent.jsx
+++ cart/src/CartContent.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 
 import { login, jwt } from "./cart";
 import Login from "./Login";
+import MiniCart from "./MiniCart";
 
 export default function CartContent() {
   const [token, setToken] = useState("");
@@ -14,6 +15,7 @@ export default function CartContent() {
     <div>
       <div>JWT: {token}</div>
       <Login />
+      <MiniCart />
     </div>
   );
 }
