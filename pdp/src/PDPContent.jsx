fd3f32a4fbe4df99c47f9216996ed61dd88a0af6
Arpit Mehrotra <arpitmehrotra001@gmail.com>
2023-02-01 08:24:05 +0530

    add the addtocart app, cross platform mfe

--- pdp/src/PDPContent.jsx
+++ pdp/src/PDPContent.jsx
@@ -1,6 +1,8 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { useParams } from "react-router-dom";
 import { getProductById, currency } from "home/products";
+
+import placeAddToCart from "addtocart/placeAddToCart";
 
 export default function PDPContent() {
   const { id } = useParams();
@@ -16,6 +18,14 @@ export default function PDPContent() {
     }
   }, [id]);
 
+  const addToCart = useRef(null);
+
+  useEffect(() => {
+    if (addToCart.current) {
+      placeAddToCart(addToCart.current, product.id);
+    }
+  }, [product]);
+
   if (!product) return null;
 
   return (
@@ -30,6 +40,7 @@ export default function PDPContent() {
             {currency.format(product.price)}
           </div>
         </div>
+        <div ref={addToCart}></div>
         <div className="mt-10">{product.description}</div>
         <div className="mt-10">{product.longDescription}</div>
       </div>
